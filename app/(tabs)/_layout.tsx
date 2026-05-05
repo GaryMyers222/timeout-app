db8079db658920266dea0dcb1228e0fac2d2fa5f
GaryMyers222 <40128195+GaryMyers222@users.noreply.github.com>
2026-05-05 08:41:38 -0700

    Add Invite tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -37,6 +37,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="list.bullet.rectangle.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="invite"
+        options={{
+          title: 'Invite',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.crop.circle.badge.plus" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
